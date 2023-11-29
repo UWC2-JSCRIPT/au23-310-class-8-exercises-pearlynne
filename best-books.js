bb1a17fd4bf7fdbdb05a4e216cf01d3e0591ced5
Pearlynne <chongpearlynnelh@gmail.com>
2023-11-29 15:16:33 -0800

    Add classList to span elements and add comments

--- best-books.js
+++ best-books.js
@@ -27,19 +27,19 @@ function getFiveBestsellers(responseJson) {
 	const booksContainer = document.getElementById('books-container')
 	booksContainer.innerText = ""
 
+	// Get first 5 bestsellers 
 	for (i = 0; i <= 5; i++) {
 
-		// Get first 5 bestsellers 
 		let book = responseJson.results.books[i];
 
-		// Create a paragraph element for each book;
+		// Create a paragraph element for each book
 		const bookPEl = document.createElement('p');
 		bookPEl.setAttribute("class", `bestseller-${i}`);
 
 		// Create Span element to identify which bestseller 
 		const spanHeader = document.createElement('span');
-		spanHeader.setAttribute("style", 'font-weight: bold');
-		spanHeader.innerHTML = `#${i + 1} hardcover fiction bestseller<br>`;
+		spanHeader.setAttribute("style", 'font-weight: bold; text-decoration: underline');
+		spanHeader.innerHTML = `#${i + 1} Hardcover Fiction Bestseller<br>`;
 
 		// Create Span element for title 
 		const spanTitle = document.createElement('span');
@@ -48,12 +48,12 @@ function getFiveBestsellers(responseJson) {
 
 		// Create Span element for author
 		const spanAuthor = document.createElement('span');
-		spanAuthor.setAttribute("class", "book-author");
+		spanAuthor.classList.add("book-author");
 		spanAuthor.innerHTML = `<b>Author</b>: ${book.author}<br>`;
 
 		// Create Span element for description
 		const spanDesc = document.createElement('span');
-		spanDesc.setAttribute("class", "book-description");
+		spanDesc.classList.add("book-description");
 		spanDesc.innerHTML = `<b>Description</b>: ${book.description} <br>`;
 
 		// Create image element for book image
@@ -72,13 +72,14 @@ function getFiveBestsellers(responseJson) {
 formEl.addEventListener('submit', function (e) {
 	e.preventDefault();
 
+	// Get date values from form
 	const year = yearEl.value;
 	const month = monthEl.value;
 	const day = dateEl.value; // edited this
 
 	// Fetch bestselling books for date and add top 5 to page
 
-	//Get date and name of book genre
+	//Get full date and name of book genre
 	const date = `${year}-${month}-${day}`
 	const name = "hardcover-fiction"
 
