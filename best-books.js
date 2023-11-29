17305966b7af6e7bb8937098d27e6528c93461fa
Pearlynne <chongpearlynnelh@gmail.com>
2023-11-29 15:06:29 -0800

    Add getFiveBestsellers function to Fetch, to get title, author, and description from JSON

--- best-books.js
+++ best-books.js
@@ -1,14 +1,58 @@
+
+// create api-key.js file with const API_KEY="your_api_key" in this same directory to use
+const BASE_URL = 'https://api.nytimes.com/svc/books/v3/lists.json';
+
+// 
+const asyncFetch = async function () {
+	try {
+		const data = await fetch(url)
+		const dataJson = await data.Json()
+		console.log("Async/Await", data)
+		console.log("Async/Await", dataJson)
+	} catch (err) {
+		console.log(err)
+	}
+}
+
+// Get elements from form 
 const formEl = document.getElementById('best-books-form');
 const yearEl = document.getElementById('year');
 const monthEl = document.getElementById('month');
 const dateEl = document.getElementById('date');
 
-formEl.addEventListener('submit', function(e) {
-  e.preventDefault();
+// Create function for Fetch to get books 
+function getFiveBestsellers(responseJson) {
+	console.log(responseJson);
 
-  const year = yearEl.value;
-  const month = monthEl.value;
-  const date = dateEl.value;
+	for (i = 0; i <= 5; i++) {
+		let book = responseJson.results.books[i];
+		const title = book.title;
+		const author = book.author;
+		const desc = book.description;
+		console.log(title, author, desc)
+	}
+}
 
-  // Fetch bestselling books for date and add top 5 to page
-});
+formEl.addEventListener('submit', function (e) {
+	e.preventDefault();
+
+	const year = yearEl.value;
+	const month = monthEl.value;
+	const day = dateEl.value; // edited this
+
+	// Fetch bestselling books for date and add top 5 to page
+
+	//Get date and name of book genre
+	const date = `${year}-${month}-${day}`
+	const name = "hardcover-fiction"
+
+	const url = `https://api.nytimes.com/svc/books/v3/lists/${date}/${name}.json?api-key=${MY_API}`;
+
+	fetch(url)
+		.then(function (data) {
+			return data.json();
+		})
+		.then(function (responseJson) {
+			getFiveBestsellers(responseJson);
+		})
+})
